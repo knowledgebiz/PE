58f0a35b9ab9eedaebda275dd5006dc770f77527
Ruben Lebre <ruben.lebre@gmail.com>
2019-06-05 11:47:07 +0100

    app.js now uses the routers.

--- current/server/src/app.js
+++ current/server/src/app.js
@@ -1,10 +1,25 @@
 const express = require('express')
 require('./db/connection')
+require('./bootstrap')()
+const answerTypeRouter = require('./routers/answerType')
+const evaluationModelRouter = require('./routers/evaluationModel')
+const competencyRouter = require('./routers/competency')
+const quantitativeObjectiveRouter = require('./routers/quantitativeObjective')
+const evaluationRouter = require('./routers/evaluation')
+const cors = require('cors')
+
 
 const port = process.env.PORT
 const app = express()
 
 app.use(express.json())
+app.use(cors())
+
+app.use('/api', answerTypeRouter)
+app.use('/api', evaluationModelRouter)
+app.use('/api', competencyRouter)
+app.use('/api', quantitativeObjectiveRouter)
+app.use('/api', evaluationRouter)
 
 app.listen(port, () => {
     console.log('Connected on port', port)
